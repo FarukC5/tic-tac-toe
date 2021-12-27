2295c4176d5db768d3d70bf1bf57c0a178542f38
FarukC5 <faruk_cajic@yahoo.com>
2021-12-27 02:45:14 +0100

    onemogucen unos ispod modala i izmjena u reset game button

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,32 +1,29 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import "./components.css";
 import EndGame from "./EndGame";
 import Navbar from "./Navbar";
 
-const Board = ({ players, newGame, trigger, setTrigger }) => {
+const Board = ({
+  players,
+  newGame,
+  trigger,
+  setTrigger,
+  gameHistory,
+  setGameHistory,
+}) => {
   const [winner, setWinner] = useState(null);
   const [turn, setTurn] = useState(players[players.length - 2]);
   const [cells, setCells] = useState(Array(9).fill(null));
   const [ties, setTies] = useState(0);
   const [count1, setCount1] = useState(0);
   const [count2, setCount2] = useState(0);
-  const [gameHistory, setGameHistory] = useState([]);
-  
-  useEffect(() => {
-    const temp2 = localStorage.getItem("gameHistory");
-    const loadedGameHistory = JSON.parse(temp2);
-    if (loadedGameHistory) {
-      setGameHistory(loadedGameHistory);
-    }
-  }, []);
-
-  useEffect(() => {
-    const temp2 = JSON.stringify(gameHistory);
-    localStorage.setItem("gameHistory", temp2);
-  }, [gameHistory]);
 
   const Cell = ({ num }) => {
-    return <td onClick={() => handleClick(num)}>{cells[num]}</td>;
+    return (
+      <td className="cell" onClick={() => handleClick(num)}>
+        {cells[num]}
+      </td>
+    );
   };
 
   const tryAgain = () => {
@@ -40,23 +37,23 @@ const Board = ({ players, newGame, trigger, setTrigger }) => {
   };
 
   const handleClick = (num) => {
-    if (cells[num] !== null) {
-      alert("Choose unoccupied cell");
-      return;
+    if (trigger === true) {
+      if (cells[num] !== null) {
+        alert("Choose unoccupied cell");
+        return;
+      }
+      let squares = [...cells];
+      if (turn === players[players.length - 2]) {
+        squares[num] = "X";
+        setTurn(players[players.length - 1]);
+      } else {
+        squares[num] = "O";
+        setTurn(players[players.length - 2]);
+      }
+      checkForWinner(squares);
+      setCells(squares);
     }
-    let squares = [...cells];
-    if (turn === players[players.length - 2]) {
-      squares[num] = "X";
-      setTurn(players[players.length - 1]);
-    } else {
-      squares[num] = "O";
-      setTurn(players[players.length - 2]);
-    }
-    checkForWinner(squares);
-    setCells(squares);
   };
-
-// CheckForWinner preuzeto sa react-ove stranice uz moje dodatke
 
   const checkForWinner = (squares) => {
     const lines = [
@@ -72,21 +69,18 @@ const Board = ({ players, newGame, trigger, setTrigger }) => {
 
     for (let i = 0; i < lines.length; i++) {
       const [a, b, c] = lines[i];
-      
-        let gameDate = (
+
+      let gameDate =
         new Date().getDate() +
-          "." +
-          (new Date().getMonth() + 1) +
-          " " + 
-          new Date().getHours() +
-          ":" +
-          new Date().getMinutes()
-      );
-      
-      
-      
+        "." +
+        (new Date().getMonth() + 1) +
+        " " +
+        new Date().getHours() +
+        ":" +
+        new Date().getMinutes();
+
       let versus =
-         gameDate +
+        gameDate +
         " " +
         players[players.length - 2] +
         " VS " +
@@ -101,7 +95,7 @@ const Board = ({ players, newGame, trigger, setTrigger }) => {
         id: new Date().toString(),
         text: versus + ": " + players[players.length - 1] + " won ",
       };
-      let isDraw = { id: new Date().toString(), text: versus + ": draw" };
+      let isDraw = { id: new Date().toString(), text: versus + ": Tie !" /* ": draw"*/ };
 
       if (
         squares[a] &&
@@ -110,47 +104,25 @@ const Board = ({ players, newGame, trigger, setTrigger }) => {
       ) {
         if (squares[a] && turn === players[players.length - 2]) {
           setWinner("You winn " + players[players.length - 2] + " !!");
-
-          // Da bi posljednji log igre bio uvijek prvi na listi nisam koristio ove metode koje su u komentarima
-
-          /*
-            setGameHistory(
-              (prevGameHistory) =>
-                prevGameHistory.concat(
-                  gameDate,
-                  versus,
-                  playerss[players.length - 2]
-                ) + " WON! "
-            );
-           */
-          // setGameHistory(gameHistory.concat(gameDate + versus +  " - " + playerss[players.length - 2]) + " WON!");
-          // setGameHistory([...gameHistory].concat(newHistory1))
           setGameHistory([newHistory1, ...gameHistory]);
           setCount1((prevCount1) => prevCount1 + 1);
-
-          return;
+          //  return;
         } else {
           setWinner("You win " + players[players.length - 1] + " !!");
           setGameHistory([newHistory2, ...gameHistory]);
           setCount2((prevCount2) => prevCount2 + 1);
 
-          return;
-          //}
+          //  return;
         }
       }
       if (!squares.includes(null)) {
-        setWinner("DRAW !");
+        setWinner("Tie !");
         setGameHistory([isDraw, ...gameHistory]);
         setTies(ties + 1);
       }
     }
-    return;
+    //   return;
   };
-
-  //  console.log(gameHistory);
-  // console.log(cells);
-  //  console.log("winner is: " + winner);
-  //  console.log("its " + turn + " turn");
 
   const List = () => {
     return (
@@ -161,11 +133,16 @@ const Board = ({ players, newGame, trigger, setTrigger }) => {
         }}
       >
         <div className="popup-inner">
-          Game History :
+          <div className="close-button">
+            <span>Game History :</span>
+            <button>X</button>
+          </div>
+          
+          &nbsp;
           {gameHistory.map((newHistory) => (
             <p
               key={newHistory.id}
-              style={{ fontSize: "12px", margin: "0px", padding: "0px" }}
+              style={{ fontSize: "90%", fontWeight:"normal" , margin: "0px", padding: "0px" }}
             >
               {newHistory.text}
             </p>
@@ -176,7 +153,7 @@ const Board = ({ players, newGame, trigger, setTrigger }) => {
   };
 
   return (
-    <div>
+    <div className="Board">
       {!trigger && <List />}
 
       {winner && (
@@ -188,55 +165,47 @@ const Board = ({ players, newGame, trigger, setTrigger }) => {
       )}
 
       {!winner && (
-        <div className="Login">
+        <div className="Board-screen">
           <Navbar
             players={players}
             count1={count1}
             count2={count2}
             ties={ties}
           />
-          <div className="Board-wrapp" id="Board">
-           <div> its <span className="turn">{turn}</span> turn</div> 
+          <div className="span12">
+            
+            <span className="span2">its</span>
+            &nbsp;
+            <span className="turn">{turn} </span>
+            &nbsp;
+            <span className="span2">turn</span>
+          </div>
+          <div className="subBoard-screen">
             <table>
               <tbody>
                 <tr>
                   <Cell num={0} />
                   <Cell num={1} />
                   <Cell num={2} />
-                </tr>
-                <tr>
                   <Cell num={3} />
                   <Cell num={4} />
                   <Cell num={5} />
-                </tr>
-                <tr>
                   <Cell num={6} />
                   <Cell num={7} />
                   <Cell num={8} />
                 </tr>
               </tbody>
             </table>
-            <button
-              className="button1"
-              onClick={() => {
-                setCells(Array(9).fill(null));
-                setWinner(null);
-                setTurn(players[players.length - 2]);
-                setTrigger(true);
-              }}
-            >
-              Reset Board
-            </button>
-            &nbsp;
-            <button
-              className="button1"
-              onClick={() => {
-                newGame();
-              }}
-            >
-              New Game
-            </button>
           </div>
+          &nbsp;
+          <button
+            className="button1"
+            onClick={() => {
+              newGame();
+            }}
+          >
+            Reset Game
+          </button>
         </div>
       )}
     </div>
